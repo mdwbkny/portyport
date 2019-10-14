cafac27c69ac1ee6e98470bf016763c25a94439f
Mike Wiesenhart <mike@mdwbkny.com>
2019-10-14 12:13:40 -0400

    Set root page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :projects
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'projects#home'
 end
